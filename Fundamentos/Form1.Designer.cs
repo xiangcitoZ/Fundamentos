43d12d0ecbe5852b638a7fdc8ffaee32fd684597
xiangcitoZ <pzxiang1@gmail.com>
2023-01-11 09:57:24 +0100

    Add project files.

---
+++ Fundamentos/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Fundamentos
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnName = new System.Windows.Forms.Button();
+            this.lbName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnName
+            // 
+            this.btnName.BackColor = System.Drawing.Color.Goldenrod;
+            this.btnName.Location = new System.Drawing.Point(167, 159);
+            this.btnName.Name = "btnName";
+            this.btnName.Size = new System.Drawing.Size(321, 65);
+            this.btnName.TabIndex = 0;
+            this.btnName.Text = "Pulsar";
+            this.btnName.UseVisualStyleBackColor = false;
+            this.btnName.Click += new System.EventHandler(this.btnName_Click);
+            // 
+            // lbName
+            // 
+            this.lbName.AutoSize = true;
+            this.lbName.Location = new System.Drawing.Point(178, 110);
+            this.lbName.Name = "lbName";
+            this.lbName.Size = new System.Drawing.Size(51, 15);
+            this.lbName.TabIndex = 1;
+            this.lbName.Text = "Nombre";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(248, 102);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(215, 23);
+            this.txtName.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lbName);
+            this.Controls.Add(this.btnName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btnName;
+        private Label lbName;
+        private TextBox txtName;
+    }
+}
---
+++ Fundamentos/Form1.cs
@@ -0,0 +1,26 @@
+namespace Fundamentos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnName_Click(object sender, EventArgs e)
+        {   
+            //AQUI ESCRIBIMOS NUESTRAS ACCIONES AL PULSAR EL BOTON
+            //TENEMOS PROPIEDAS DE TIPO PRIMITIVO
+            this.txtName.Width = 150;
+            this.btnName.Text = "Botón pulsado!!";
+            //TENEMOS PROPIEDADES QUE SON DE TIPO OBJETO
+            this.txtName.Location = new Point(70, 100);
+            //TENEMOS PROPIEDADES DE TIPO ENUMERADAS
+            //UNA ENUMERACION SON UNA SERIE DE VALOR QUE
+            //SE REPRESENTAN DE FORMA GRAFICA Y AYUDAN AL PROGRAMADOR
+            this.txtName.TextAlign = HorizontalAlignment.Center;
+            this.BackColor = Color.Beige;
+
+        }
+    }
+}
